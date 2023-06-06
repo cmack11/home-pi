60611a450a79a6c52f948ffdce4029c5042ca2c9
cmack11 <claymackenthun@yahoo.com>
2023-06-05 21:37:57 -0500

    type error

--- controllers/mail/utilities.ts
+++ controllers/mail/utilities.ts
@@ -24,7 +24,7 @@ export const parseMail = (mail: any) => {
 	} else if(sender?.match(/@vzwpix.com/)) {
 		message = attachments?.[0]?.content?.toString();
 	} else if(sender?.match(/@tmomail.net/)) {
-		message = attachments?.some(a => a?.contentType === "text/plain")?.content?.toString();
+		message = attachments?.some((a: any) => a?.contentType === "text/plain")?.content?.toString();
 		if(!message) {
 			message = text?.replaceAll(/\[cid:.*?\]/g, "")?.split("\n").pop();
 		}
